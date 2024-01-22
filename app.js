942467f74f713c12e55df12085475040cf0e52d5
rubcube-guilhermetomiasi <guilherme.tomiasi@rubcube.com>
2024-01-22 14:40:24 -0300

    style: formatting current files with new configuration

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
-import 'dotenv/config';
-import express from 'express';
+import "dotenv/config";
+import express from "express";
 import {
-  InteractionType,
-  InteractionResponseType,
-  InteractionResponseFlags,
-  MessageComponentTypes,
-  ButtonStyleTypes,
-} from 'discord-interactions';
-import { VerifyDiscordRequest, getRandomEmoji, DiscordRequest } from './utils.js';
-import { getShuffledOptions, getResult } from './game.js';
-import FirebaseHandler from './firebase/firebase-connector.js';
-import RolesHandlerFirebase from './roles/roles-handler-firebase.js';
-import DiscordHtttpClient from './discord/discord-http-client.js';
-
+    InteractionType,
+    InteractionResponseType,
+    InteractionResponseFlags,
+    MessageComponentTypes,
+    ButtonStyleTypes,
+} from "discord-interactions";
+import { VerifyDiscordRequest, getRandomEmoji, DiscordRequest } from "./utils.js";
+import { getShuffledOptions, getResult } from "./game.js";
+import FirebaseHandler from "./firebase/firebase-connector.js";
+import RolesHandlerFirebase from "./roles/roles-handler-firebase.js";
+import DiscordHtttpClient from "./discord/discord-http-client.js";
 
 // Create an express app
 const app = express();
@@ -32,53 +31,52 @@ RolesHandlerFirebase.getInstance();
 /**
  * Interactions endpoint URL where Discord will send HTTP requests
  */
-app.post('/interactions', async function (req, res) {
-  // Interaction type and data
-  const { type, id, data } = req.body;
+app.post("/interactions", async function (req, res) {
+    // Interaction type and data
+    const { type, id, data } = req.body;
 
-  /**
-   * Handle verification requests
-   */
-  if (type === InteractionType.PING) {
-    return res.send({ type: InteractionResponseType.PONG });
-  }
-
-  /**
-   * Handle slash command requests
-   * See https://discord.com/developers/docs/interactions/application-commands#slash-commands
-   */
-  if (type === InteractionType.APPLICATION_COMMAND) {
-    const { name } = data;
-
-    // "test" command
-    if (name === 'test') {
-      // Send a message into the channel where command was triggered from
-      return res.send({
-        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-        data: {
-          // Fetches a random emoji to send from a helper function
-          content: 'hello world ' + getRandomEmoji(),
-        },
-      });
+    /**
+     * Handle verification requests
+     */
+    if (type === InteractionType.PING) {
+        return res.send({ type: InteractionResponseType.PONG });
     }
-  }
+
+    /**
+     * Handle slash command requests
+     * See https://discord.com/developers/docs/interactions/application-commands#slash-commands
+     */
+    if (type === InteractionType.APPLICATION_COMMAND) {
+        const { name } = data;
+
+        // "test" command
+        if (name === "test") {
+            // Send a message into the channel where command was triggered from
+            return res.send({
+                type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+                data: {
+                    // Fetches a random emoji to send from a helper function
+                    content: "hello world " + getRandomEmoji(),
+                },
+            });
+        }
+    }
 });
 
-app.get('/oauth', async function(req, res) {
-  const {code} = req.query;
+app.get("/oauth", async function (req, res) {
+    const { code } = req.query;
 
-  if(code) {
+    if (code) {
+        const discorHttpClient = DiscordHtttpClient.getInstance();
 
-    const discorHttpClient = DiscordHtttpClient.getInstance();
+        const output = await discorHttpClient.oauth(code);
 
-    const output = await discorHttpClient.oauth(code);
+        return res.send(output);
+    }
 
-    return res.send(output);
-  }
-
-  return res.send({error: "code not provided."})
-})
+    return res.send({ error: "code not provided." });
+});
 
 app.listen(PORT, () => {
-  console.log('Listening on port', PORT);
+    console.log("Listening on port", PORT);
 });
--- discord/discord-http-client.js
+++ discord/discord-http-client.js
@@ -1,19 +1,18 @@
 import axios from "axios";
 import AxiosCurlirize from "axios-curlirize";
-import url from 'url'
+import url from "url";
 
 export default class DiscordHtttpClient {
-    
     static instance;
 
     constructor() {
         this.axios = axios.create({
-            baseURL: process.env.DISCORD_API_HOST
-        })
+            baseURL: process.env.DISCORD_API_HOST,
+        });
     }
 
     static getInstance() {
-        if(!this.instance) {
+        if (!this.instance) {
             this.instance = new DiscordHtttpClient();
         }
 
@@ -24,20 +23,15 @@ export default class DiscordHtttpClient {
         const formData = new url.URLSearchParams({
             client_id: process.env.APP_ID.toString(),
             client_secret: process.env.CLIENT_SECRET.toString(),
-            grant_type: 'authorization_code',
+            grant_type: "authorization_code",
             code: code.toString(),
             //redirect_uri: process.env.OAUTH_REDIRECT_URI.toString()
-        })
+        });
         AxiosCurlirize(this.axios);
-        return await this.axios.post(
-            "/oauth2/token",
-            formData,
-            {
-                headers: {
-                    'Content-Type': "application/x-www-form-urlencoded"
-                }
-            }
-        )
+        return await this.axios.post("/oauth2/token", formData, {
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+            },
+        });
     }
-    
 }
